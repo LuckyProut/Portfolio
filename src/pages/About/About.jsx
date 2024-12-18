6e44734a63781a699ef4f19c1b35478d5e63bd33
LuckyProut <luc.garrouste@gmail.com>
2024-12-18 17:36:11 +0100

    add responsive tablet + mobile all pages except project

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import Portrait from '../../img/portrait.png'
+import sassIMG from '../../img/sass.webp'
+import htmlIMG from '../../img/HTML5.webp'
+import cssIMG from  '../../img/CSS3.webp'
+import nodeIMG from  '../../img/Node.webp'
+import reactIMG from  '../../img/react.webp'
+import nosqlIMG from  '../../img/NoSql.png'
+import jsIMG from  '../../img/js.webp'
 import '../../style/about.scss';
 import ContactForm from '../../components/form/form';
 
@@ -15,11 +22,42 @@ const About = () => {
             Après avoir travaillé pendant presque 10 ans dans la restauration, j'ai pris la décision d'entamer une reconversion professionnelle en débutant une formation visant à m'aider à atteindre mes objectifs.
           </p>
           <p>
-            En l'espace de 6 mois,, grâce à la formation Développeur Web d'OpenClassroom, j'ai réussi à me former dans plusieurs domaines, que ça soit du côté du front-end (HTML, CSS, JS, etc...) ou bien du back-end (Node.js, Express, Base de données, etc...).
+            En l'espace de 6 mois, grâce à la formation Développeur Web d'OpenClassroom, j'ai réussi à me former dans plusieurs domaines, que ça soit du côté du front-end (HTML, CSS, JS, etc...) ou bien du back-end (Node.js, Express, Base de données, etc...).
             Je continue, en autodidacte, à me former à de nouvelles technologies qui me permettraient de continuer dans ma lancer d'évolution dans mes compétences.
           </p>
         </div>
-      </div><ContactForm /></>
+      </div>
+      <h2 className='competence__title'>Mes Compétences</h2>
+      <div className='competence'>
+        <div className='competence__figure'>
+          <img src={htmlIMG} alt='logo HMTL5'></img>
+          <p className='competence__name'>HTML</p>
+        </div>
+        <div className='competence__figure'>
+          <img src={cssIMG} alt='logo CSS3'></img>
+          <p className='competence__name'>CSS</p>
+        </div>
+        <div className='competence__figure'>
+          <img src={jsIMG} alt='logo JavaScript'></img>
+          <p className='competence__name'>JavaScript</p>
+        </div>
+        <div className='competence__figure'>
+          <img src={reactIMG} alt='logo React.js'></img>
+          <p className='competence__name'>React.js</p>
+        </div>
+        <div className='competence__figure'>
+          <img src={sassIMG} alt='logo SASS'></img>
+          <p className='competence__name'>SASS</p>
+        </div>
+        <div className='competence__figure'>
+          <img src={nodeIMG} alt='logo NODE.js'></img>
+          <p className='competence__name'>Node.js</p>
+        </div>
+        <div className='competence__figure'>
+          <img src={nosqlIMG} alt='logo NoSQL'></img>
+          <p className='competence__name'>NoSQL</p>
+        </div>
+        </div><ContactForm /></>
     );
   };
   
--- src/pages/Projet/Projet.jsx
+++ src/pages/Projet/Projet.jsx
@@ -3,8 +3,6 @@ import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import Data from '../../data/data.json';
-import Collapse from '../../components/collapse/collapse';
-import Tags from '../../components/tag/tag';
 
 function Projet() {
    
