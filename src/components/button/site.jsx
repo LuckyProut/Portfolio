a0b9226decba1694c84c9f08367ba550ac255cb0
LuckyProut <luc.garrouste@gmail.com>
2024-12-23 05:05:14 +0100

    modify button -> if no website url hide button

--- src/components/button/site.jsx
+++ src/components/button/site.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import '../../style/button.scss';
 
 const ButtonSite = ({ siteurl, image }) => {
+  if (!siteurl || !image) {
+    console.warn('Props "siteurl" or "image" is missing in ButtonSite');
+    return null; // Ne rien afficher si les props sont manquantes
+  }
+
   return (
-    <button className='button__url'
-      onClick={() => window.open(siteurl, '_blank')} 
+    <button
+      className="button__url"
+      onClick={() => window.open(siteurl, '_blank')}
     >
-        <img src={image}/>
+      <img src={image} alt="Button icon" />
     </button>
   );
 };
+
 
 
 
